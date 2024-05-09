f3a8ab217e7a5a784a059e7d495b3e2c93902c55
atikxcode <liadbd20@gmail.com>
2024-05-10 00:13:17 +0600

    firebase initialized

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,40 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Layout/Root';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
+import AuthProvider from './Providers/AuthProvider';
+import NavBar from './Pages/NavBar/NavBar';
+import Footer from './Pages/Footer/Footer';
+import Home from './Pages/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/navbar',
+        element: <NavBar></NavBar>
+      },
+      {
+        path: '/footer',
+        element: <Footer></Footer>,
+      },
+      {
+        path: '/',
+        element: <Home></Home>,
+      }
+    ]
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   <AuthProvider>
     <RouterProvider router={router} />
+
+    </AuthProvider>
   </React.StrictMode>,
 )
